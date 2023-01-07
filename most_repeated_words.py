2df347f4bad2d1f81c49cdc05659064a8ebdde5c
Mahesh N D <98675386+MaheshND-321@users.noreply.github.com>
2023-01-07 06:37:44 +0530

    second_commit

--- most_repeated_words.py
+++ most_repeated_words.py
@@ -1,3 +1,53 @@
+# Video to Audio Convertion
+##################################################################
+# Author : Arnab Basak
+
+import os
+import imageio
+import moviepy.editor
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw()
+filelocation = askopenfilename()
+
+print('Conversion in progress...')
+
+basename = os.path.basename(filelocation)
+
+filename = os.path.splitext(basename)[0]
+
+video = moviepy.editor.VideoFileClip(filelocation)
+audio = video.audio
+
+aud = audio.write_audiofile(filename+".wav")
+print(aud)
+
+print('Conversion Done Successfully!')
+
+
+# Audio to text convertion
+########################################################################
+#import library
+import speech_recognition as sr
+#Initiаlize  reсоgnizer  сlаss  (fоr  reсоgnizing  the  sрeeсh)
+r = sr.Recognizer()
+# Reading Audio file as source
+#  listening  the  аudiо  file  аnd  stоre  in  аudiо_text  vаriаble
+with sr.AudioFile('short.wav') as source:
+    audio_text = r.listen(source)
+# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+    try:
+        # using google speech recognition
+        text = r.recognize_google(audio_text)
+        print('Converting audio transcripts into text ...')
+        print(text)
+    except:
+         print('Sorry.. run again...')
+
+
+# printing most repitative words in the text
+####################################################################################
 text = 'Ramanujan spoke about black holes nearly 100 years or more than hundred years ago there was no concept of black mathematics for black holes when there was no concept of Black Hole science always progress is like this but the concept then the theory and then the mathematics but he made the mathematics first before before their eyes when they are and still more in our mathematics notebooks and netbooks mathematics sim ki foreigner my baby please mathematics'
 count=0
 maxcount = 0
