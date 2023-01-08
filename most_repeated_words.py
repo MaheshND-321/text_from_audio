29e0c18b0176d88ef0e2c8ce8e325af7bd63cda0
Mahesh N D <98675386+MaheshND-321@users.noreply.github.com>
2023-01-08 09:34:09 +0530

    commits

--- most_repeated_words.py
+++ most_repeated_words.py
@@ -1,11 +1,9 @@
-# video to Audio conversion
-###################################################
+
 import os
 import imageio
 import moviepy.editor
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-import speech_recognition as sr
 
 Tk().withdraw()
 filelocation = askopenfilename()
@@ -19,30 +17,28 @@ filename = os.path.splitext(basename)[0]
 video = moviepy.editor.VideoFileClip(filelocation)
 audio = video.audio
 
-audio.write_audiofile(filename+".wav")
-# print(aud)
+aud = audio.write_audiofile(filename+".wav")
+print(aud)
 
 print('Conversion Done Successfully!')
 
-# Audio to text conversion
-######################################################################
-#Initiаlize  reсоgnizer  сlаss  (fоr  reсоgnizing  the  sрeeсh)
+
+#######################################################################################################
+
+import speech_recognition as sr
+
 r = sr.Recognizer()
-# Reading Audio file as source
-#  listening  the  аudiо  file  аnd  stоre  in  аudiо_text  vаriаble
-with sr.AudioFile('handsome.wav') as source:
-    audio_text = r.listen(source)
-# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+
+with sr.AudioFile(str(filename+".wav")) as source:
+    audio = r.listen(source)
     try:
-        # using google speech recognition
-        text = r.recognize_google(audio_text)
-        print('Converting audio transcripts into text ...')
+        text =r.recognize_google(audio)
+        print("working on....")
         print(text)
     except:
-         print('Sorry.. run again...')
+        print('Sorry... pleaser try again....')
 
 
-# printing the frequently occuring words in the text
 #################################################################
 # Python program to find the k most frequent words
 # from data set
@@ -58,6 +54,9 @@ Counter = Counter(split_it)
 
 # most_common() produces k frequently encountered
 # input values and their respective counts.
-most_occur = Counter.most_common(6)
+most_occur = Counter.most_common(20)
 
-print("The Most Frequently occuring words in the video is : ",most_occur)
+print("The Most Frequently occuring words in the video is : ")
+for item in most_occur:
+    if len(item[0])>=5:
+        print(item)
